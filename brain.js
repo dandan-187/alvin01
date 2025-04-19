c70f51d5adc4508df50a3e34f1f794560312d73e
dandan-187 <danterg187@gmail.com>
2025-04-20 00:37:00 +0800

    Delete brain.js

--- brain.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("clearButton").addEventListener("click", function() {
-    let inputs = document.querySelectorAll("input");
-    inputs.forEach(input => input.value = "");
-});
